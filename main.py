22e1338cf03d424153e9aca4ab614fb390803def
christopher-chris <christopher.chris.ing@gmail.com>
2021-06-03 13:14:28 +0100

    Add method to view a Task's details
    get_task_details() can be called on a Task object to view each constraint's output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from task_model import TaskModel
 from task_pipeline.pipeline import Pipeline
 from task_main.task import *
@@ -10,67 +11,130 @@ from constraints.models.example_models.pause_thread import PauseModel
 from constraints.models.example_models.test_combined_constraint import TestCombinedConstraintModel
 from stage.stage import Stage
 from utils.update import Observer
+from constraints.models.example_models.internet_model import InternetModel
 
 
-constraint1 = CustomConstraint("test1", TestModel())
-constraint1.add_input(4)
-constraint2 = CustomConstraint("test2", TestModel())
+# constraint1 = CustomConstraint("test1", TestModel())
+# constraint1.add_input(4)
+# constraint2 = CustomConstraint("test2", TestModel())
 
-constraint3 = CustomConstraint("test3", TestModel())
-constraint3.add_input(4)
+# constraint3 = CustomConstraint("test3", TestModel())
+# constraint3.add_input(4)
 
-time_constraint = CustomConstraint(
-    "time", PauseModel()
-)
+# time_constraint = CustomConstraint(
+#     "time", PauseModel()
+# )
 
-task_constraint = CustomConstraint("task constraint", TaskModel())
+# task_constraint = CustomConstraint("task constraint", TaskModel())
 
-combined_constraint = CustomConstraint(
-    "combined constraint", TestCombinedConstraintModel())
-combined_constraint.add_input(constraint1)
-combined_constraint.add_input(constraint2)
+# combined_constraint = CustomConstraint(
+#     "combined constraint", TestCombinedConstraintModel())
+# combined_constraint.add_input(constraint1)
+# combined_constraint.add_input(constraint2)
 
-stage = Stage("PENDING")
-# stage.add_constraint(time_constraint)
-stage.add_constraint(combined_constraint)
+# stage = Stage("PENDING")
+# # stage.add_constraint(time_constraint)
+# stage.add_constraint(combined_constraint)
 
-stage_group = StageGroup()
-stage_group.add_stage(stage)
-# stage_group.add_stage(stage2)
-
-
-shoes_entry = Entry()
-shoes_entry.add_entry("name", "Nike")
-shoes_entry.add_entry("size", 9)
-shoes_entry.add_entry("color", "blue")
-
-my_inventory = Inventory()
-my_inventory.add_entry(shoes_entry)
-
-new_task = ProductTask("Test task", "Create task")
-new_task.set_inventory(my_inventory)
-new_task.set_constraint_stage_config(stage_group)
-new_task.set_mode_of_execution(ModeOfExecution.ONLINE)
-new_task.set_price_constraint(combined_constraint)
-
-pipeline = Pipeline(new_task, new_task.constraint_stage_config, False)
-new_task.add_constraint_to_stage(constraint3, "PENDING")
-
-# pipeline.log()
+# stage_group = StageGroup()
+# stage_group.add_stage(stage)
+# # stage_group.add_stage(stage2)
 
 
-# pipeline.add_input_to_constraint("time", "PENDING", 33)
+# shoes_entry = Entry()
+# shoes_entry.add_entry("name", "Nike")
+# shoes_entry.add_entry("size", 9)
+# shoes_entry.add_entry("color", "blue")
+
+# my_inventory = Inventory()
+# my_inventory.add_entry(shoes_entry)
+
+# new_task = ProductTask("Test task", "Create task")
+# new_task.set_inventory(my_inventory)
+# new_task.set_constraint_stage_config(stage_group)
+# new_task.set_mode_of_execution(ModeOfExecution.ONLINE)
+# new_task.set_price_constraint(combined_constraint)
+
+# pipeline = Pipeline(new_task, new_task.constraint_stage_config, False)
+# new_task.add_constraint_to_stage(constraint3, "PENDING")
+
+# # pipeline.log()
 
 
-def react(pipe, args):
-    print("-", pipe.current_stage.log.most_recent_update)
-    pass
+# # pipeline.add_input_to_constraint("time", "PENDING", 33)
 
 
-pipeline.on_constraint_complete(
-    react, "sd", "sdf")
+# def react(pipe, args):
+#     print("-", pipe.current_stage.log.most_recent_update)
+#     pass
 
-# pipeline.start()
-pipeline.start_stage("PENDING")
-pipeline.start_constraint("PENDING", "combined constraint")
-pipeline.start_constraint("PENDING", "test3")
+
+# pipeline.on_constraint_complete(
+#     react, "sd", "sdf")
+
+# # pipeline.start()
+# pipeline.start_stage("PENDING")
+# pipeline.start_constraint("PENDING", "combined constraint")
+# pipeline.start_constraint("PENDING", "test3")
+
+def createCon1():
+    return CustomConstraint("con", "desc", InternetModel(), debug=False)
+
+
+def createCon2():
+    return CustomConstraint("con2", "desc", InternetModel(), debug=False)
+
+
+constraints = {
+    "con": createCon1(),
+    "con2": createCon2()
+}
+
+
+cons = createCon1()
+cons1 = constraints["con2"]
+cons.add_input("EUR")
+cons.add_input("USD")
+
+cons1.add_input("NGN")
+cons1.add_input("USD")
+
+s = Stage('s')
+s.add_constraint(cons)
+
+s2 = Stage('s2')
+cons = createCon1()
+cons.add_input("EUR")
+cons.add_input("USD")
+cons1 = constraints["con2"]
+# cons1.add_input("EUR")
+# cons1.add_input("USD")
+s2.add_constraint(cons)
+s2.add_constraint(cons1)
+
+sg = StageGroup()
+sg.add_stage(s)
+sg.add_stage(s2)
+# sg.add_stage(s2)
+
+# sg.start('s')
+
+
+def update(pipe, args):
+    print(pipe.current_stage.log.most_recent_update)
+# s.start_constraint("con")
+# s.start_constraint("con2")
+
+
+task = Task("name", "desc")
+task.set_constraint_stage_config(sg)
+
+pipe = Pipeline(task, sg)
+pipe.start()
+# pipe.on_update(update)
+pipe.start_constraint("s", "con")
+sleep(2)
+pipe.start_constraint("s2", "con")
+pipe.start_constraint("s2", "con2")
+
+print(task.get_task_details())
--- task_main/task.py
+++ task_main/task.py
@@ -4,7 +4,7 @@ from task_main.enums.task_type import *
 from task_main.enums.mode_of_execution import *
 from abc import ABC
 from constraints.constraint_main.constraint import *
-from task_main.enums.stage_status import StageStatus
+from constraints.enums.stage_status import StageStatus
 from stage.stage import StageGroup
 
 
@@ -47,10 +47,11 @@ class Task(ABC):
 
     def change_desc(self, desc):
         self.description = desc
-    
+
     def add_constraint_to_stage(self, constraint, stage_name):
-        self.constraint_stage_config._get_stage_with_name(stage_name).add_constraint(constraint)
-    
+        self.constraint_stage_config._get_stage_with_name(
+            stage_name).add_constraint(constraint)
+
     def add_stage(self, stage):
         self.constraint_stage_config.add_stage(stage)
 
@@ -63,3 +64,22 @@ class Task(ABC):
 
     def set_price_constraint(self, price_constraint: Constraint):
         self.price_constraint = price_constraint
+
+    def get_task_details(self):
+        details = []
+        for stage in self.constraint_stage_config.stages:
+            if stage.status == StageStatus.COMPLETE:
+                stage_data = {"stage_name": stage.name,
+                              "status": "complete", "constraint_data": []}
+                for con in stage.constraints:
+                    stage_data["constraint_data"].append(
+                        {"constraint_name": con.name, "data": con.model.output})
+                details.append(stage_data)
+            elif stage.status == StageStatus.ACTIVE:
+                stage_data = {"stage_name": stage.name,
+                              "status": "active", "constraint_data": []}
+                details.append(stage_data)
+            elif stage.status == StageStatus.NOT_STARTED:
+                stage_data = {"stage_name": stage.name,
+                              "status": "not_started", "constraint_data": []}
+        return details
